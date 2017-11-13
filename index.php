f8ecf7e6ff4852a28045cb4a74b942ce6f910be1
Unknown <iregnier@orange.fr>
2017-11-13 15:53:11 +0100

    ajout picto

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
-<?php require('parts/header.php'); ?>
+<?php require_once('parts/header.php'); ?>
 
-<h3>Bonjour et bienvenue</h3>
-<footer>
-<?php require('parts/footer.php'); ?>
-</footer>
+<link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,700,800,900" rel="stylesheet"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/styleBiscotto.css">
+
+<section>
+	
+<body>
+	<h1 id="titre" class="container-fluid"><img src="img/PictoHaltere.png" class="pictoOuv">Bienvenue sur <span>Biscotto 3000</span></h1>
+	
+	<p class="container-fluid txt">Faites du sport avec des équipements haut de gamme à moindre coût</p>
+</body>
+</section>
+
+<?php require_once('parts/footer.php'); ?>
