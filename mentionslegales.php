75d2d7afa762958a4378e9691d5bd00e3cffa1fd
lmazenc <33620952+lmazenc@users.noreply.github.com>
2017-11-14 11:45:26 +0100

    Laurent, amélioration mentions légales

--- mentionslegales.php
+++ mentionslegales.php
@@ -8,11 +8,14 @@
 	</head>
 	<body>
 		<div class="container">
-			<div class="row justify-content-md-center">
-				<div class="col-md-12">
+			<div class="row justify-content-md-center ">
+			</br>
+				<div class="col-md-12 fonddiv text-center">
 	<h1>Nos mentions légales</h1>
+	<div class="entoure">
 	<h2 class="nom">Biscotto 3000</h2>
 	<h3 class="slogan">La gonflette utile</h3>
+	</div>
 	<p>Tel: 08.36.65.65.65</p>
 	<p>5 terres du Cimetière, 87160 Arnac-la-Poste</p>
 	<p><div class="bleue">Biscotto 3000</div> est une EURL au capital de 35000€</p>
@@ -20,6 +23,7 @@
 	<p>N° déclaration CNIL : 55555</p>
 
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2758.2213654481848!2d1.3776557158066858!3d46.265704887624246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47fbfc650632d4f9%3A0xf4fb87b97e2a9148!2sTerres+du+Cimeti%C3%A8re%2C+87160+Arnac-la-Poste!5e0!3m2!1sfr!2sfr!4v1510653556228" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
 	</div>
 </div>
 </div>
