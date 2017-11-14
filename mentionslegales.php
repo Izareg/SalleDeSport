3720159c4b7924f9d8d0ffd335a65bbd31a508e0
lmazenc <33620952+lmazenc@users.noreply.github.com>
2017-11-14 08:54:31 +0100

    Add files via upload

--- mentionslegales.php
+++ mentionslegales.php
@@ -1,7 +1,21 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/stylementions.css">
+	</head>
+	<body>
 <aside class="sidebar">
 	<h1>Contact</h1>
 	<p>Tel: 08.36.65.65.65</p>
-	<p>Préfecture d'Osaka</p>
+	<p>15 Jihad Street, Bagdad</p>
 	<p>Post hanc adclinis Libano monti Phoenice, regio plena gratiarum et venustatis, urbibus decorata magnis et pulchris; in quibus amoenitate celebritateque nominum Tyros excellit, Sidon et Berytus isdemque pares Emissa et Damascus saeculis condita priscis.</p>
-	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d420029.7907150512!2d135.14071665518824!3d34.66776396547138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e86b2acc70d7%3A0xa399ff48811f596d!2sPr%C3%A9fecture+d&#39;Osaka%2C+Japon!5e0!3m2!1sfr!2sfr!4v1510134133119" width="350" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3332.8195022683753!2d44.39564871553145!3d33.34966506187241!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x155778a2fbc1ae8b%3A0x4a0c4a586cb3ab50!2sJihad+Street%2C+Baghdad%2C+Irak!5e0!3m2!1sfr!2sfr!4v1510645579748" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 </aside>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="../js/header.js"></script>
+</body>
+
