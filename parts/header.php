7982f8b33c69c84911537cab49c2f0804924e831
Jocelyn Rabahi <rabahi.jocelyn@gmail.com>
2017-11-14 08:46:58 +0100

    change col md 12

--- parts/header.php
+++ parts/header.php
@@ -6,6 +6,8 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link rel="stylesheet" type="text/css" href="css/style-nav.css">
+		<link rel="stylesheet" type="text/css" href="../login/css/login.css">
+
 	</head>
 	<body>
 		<header class="text-center">
@@ -88,4 +90,4 @@
 		</header>
 
 		<main class="container-fluid">
-			<div class="col-md-9">
+			<div class="col-md-12">
