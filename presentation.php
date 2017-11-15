a9f4e5b2b7f9b7d25dcdfdd9a212fa8f750ac2b8
Yoann Jeannin <jeannin.yoann.pro@gmail.com>
2017-11-15 12:27:07 +0100

    Rajout d'une page présentation.php

---
+++ presentation.php
@@ -0,0 +1,62 @@
+<?php require_once('parts/header.php'); ?>
+
+<link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,700,800,900" rel="stylesheet"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/presentation-style.css">
+
+	
+<!-- Titre -->
+<body>
+	<div class="container bla">
+		<div class="row">
+			<div class="col-md-12">
+			<h1 id="titre">Bienvenue chez <span>Biscotto 3000</span> <img class="logo" src="img/bisco.png"></h1>
+			<p class="txt">En campagne ou ville, osez le style, optez pour la gonflette utile !</p>
+			</div>
+		</div><!-- fermeture class row -->
+	</div>
+
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12 mes_images">
+
+				<div class="col-md-4">
+					<img src="img/6.jpg" alt="sport">
+					<figcaption>Foot en salle</figcaption>
+				</div>
+
+				<div class="col-md-4">
+					<img src="img/7.jpg" alt="sport">
+					<figcaption>Notre salle de course</figcaption>
+				</div>
+
+				<div class="col-md-4">
+					<img src="img/4.jpg" alt="sport">
+					<figcaption>Notre patinoire olympique</figcaption>
+				</div>
+			</div>
+		</div>
+	</div>			
+
+
+
+		<section>
+				<div class="container col-md-12">
+					<div class="row">
+						<div class="col-md-7">
+							<p class="paragraphe">
+								Bonjour et bienvenue sur notre site internet.<br> Notre salle se situe à Dijon, en plein coeur du centre ville, dans la caves de la mairie, pratique d'accès et grande par sa taille bla bla bla le sang de tes morts
+								</p>
+						</div>
+
+						<div class="col-md-5">
+							<aside class="blocDroit">
+								<?php require_once('login.html') ?>
+							</aside>
+						</div>
+					</div>
+				</div> <!--fermeture div class row-->
+		</section>
+</body>
+<?php require_once('parts/footer.php'); ?>
