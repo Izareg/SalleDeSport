4b22fbceffd4e15c8304dd240d2005c096b88c9f
lmazenc <33620952+lmazenc@users.noreply.github.com>
2017-11-16 14:43:37 +0100

    ajout du footer dans mentions Légales

--- mentionslegales.php
+++ mentionslegales.php
@@ -35,5 +35,5 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		<script type="text/javascript" src="../js/header.js"></script>
 
-</body>
+<?php require_once('parts/footer.php'); ?>
 
